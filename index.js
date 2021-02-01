d6ca6516bda1056c7619cdd0f3efdbb0b7855bd2
ellevuarts <ellevuarts@gmail.com>
2021-02-01 15:48:15 +0100

    aggiungo get user

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var app = require('express')();
 
-
 const about = (req, res) => {
 	res.send('bout')
 }
 
 const user = (req, res) => {
-	res.send(` Hello ${req.paramas.name}`)
+	res.send(` Hello ${req.params.name}`)
 }
 
 
@@ -14,8 +13,10 @@ app.get("/", (req, res) => {
 	res.send("hello jenkins");
 })
 
-app.get('/about', about)
-app.get('/user/:name', user)
+app.get('/about', about);
+
+app.get('/user/:name', user);
+
 app.listen(3000, () => { 
 	console.log('listening at 3000');
 });
